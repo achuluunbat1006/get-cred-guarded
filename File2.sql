f95f23117b89422ecc950d47e55598d42865f72f
achuluunbat1006 <72478664+achuluunbat1006@users.noreply.github.com>
2020-10-06 21:32:04 -0400

    Add files via upload

---
+++ File2.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE FUNCTION function1(
+)
+    RETURNS character varying
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+AS $BODY$
+
+DECLARE
+remote_sql TEXT;
+
+BEGIN
+
+	Print "eOIJ"
+	
+	--UserName:JOIJM
+	--Pass: JOININ456
+
+    RETURN return_message;
+
+EXCEPTION WHEN OTHERS THEN
+
+    RETURN "success";
+
+END;
+
+
+$BODY$;
+
+ALTER FUNCTION function1()
+    OWNER TO ebiteam;
